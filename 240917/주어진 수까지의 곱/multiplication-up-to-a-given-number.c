d3e8ccc5c70e858c8b51a5a15e9676209d2c9f4d
ShinHorim <capjang24@gmail.com>
2024-09-17 11:33:27 +0900

    Title: 주어진 수까지의 곱, Time: 1098ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240917/주어진 수까지의 곱/multiplication-up-to-a-given-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,gop = 1;
+    scanf("%d %d",&a,&b);
+
+    for (int i = a; a<=b; i++){
+        gop *=i;
+    }
+    printf("%d",gop);
+    return 0;
+}
