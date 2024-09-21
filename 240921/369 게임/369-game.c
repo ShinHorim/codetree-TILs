921fd1bb9357e15c9bda73d4a6e2378ad6e8e102
ShinHorim <capjang24@gmail.com>
2024-09-21 19:22:32 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240921/369 게임/369-game.c
+++ 240921/369 게임/369-game.c
@@ -16,8 +16,11 @@ int main() {
         else if (i%3==0 ||i/10==3|| i/10==6|| i/10==9){
             printf("0 ");
         }
+        else if (i%10 == 3|| i%10==6|| i%10==9){
+            printf("0 ");
+        }
         else{
-            printf("%d ", i);
+            printf("%d ",i);
         }
     }
     return 0;
