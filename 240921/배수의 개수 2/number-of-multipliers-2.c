fc91546b9e2306436d87c5e8c5a35519a086407d
ShinHorim <capjang24@gmail.com>
2024-09-21 19:55:11 +0900

    Title: 배수의 개수 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240921/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int hol = 0;
+    for (int i = 1; i<=10; i++){
+        scanf("%d",&a);
+        if (i%2 != 0){
+            hol += 1;
+        }
+    }
+    printf("%d", hol);
+
+    return 0;
+}
