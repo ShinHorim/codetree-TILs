1b7c3536dadea3c5915ca6c42fa87fa9c537d920
ShinHorim <capjang24@gmail.com>
2024-09-21 19:13:24 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240921/369 게임/369-game.c
+++ 240921/369 게임/369-game.c
@@ -9,7 +9,7 @@ int main() {
     il = a%10;
     ship = a/10;
     for (int i = 1; i<= a; i++){
-        if (i%3==0){
+        if (i%3==0 ||ship==3|| ship==6|| ship==9){
             printf("0 ");
         }
         else{
