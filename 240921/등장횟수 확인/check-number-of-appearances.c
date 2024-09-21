7165e2c44e54cf38655b1e1e373ffc7ab884dd8d
ShinHorim <capjang24@gmail.com>
2024-09-21 20:24:45 +0900

    Title: 등장횟수 확인, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240921/등장횟수 확인/check-number-of-appearances.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    int j = 0;
+    for(int i = 1; i<=5; i++){
+        scanf("%d". &a);
+        if (a%2==0){
+            j+=1;
+        }
+    }
+    printf("%d",j);
+    return 0;
+}
