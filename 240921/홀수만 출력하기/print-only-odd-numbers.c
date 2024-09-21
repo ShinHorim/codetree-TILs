dc88a53804c920e6830c83cc7ed3577d63cff951
ShinHorim <capjang24@gmail.com>
2024-09-21 19:49:15 +0900

    Title: 홀수만 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240921/홀수만 출력하기/print-only-odd-numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+
+   
+
+    for (int i = 1; i<= a; i++){
+        scanf("%d",&a);
+        if (a%2 != 0 && a%3==0){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
