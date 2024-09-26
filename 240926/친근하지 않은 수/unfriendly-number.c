bf87b16ce69db1a03670a39cb340684ca8975b94
ShinHorim <capjang24@gmail.com>
2024-09-26 14:17:13 +0900

    Title: 친근하지 않은 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240926/친근하지 않은 수/unfriendly-number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,sum=0;
+    scanf("%d".&a);
+
+    for(int i = 1; i<=a; i++){
+        if (i%2==0 || i%3==0|| i%5==0){
+            continue;
+        }
+        else{
+            sum += 1;
+        }
+    }
+    printf("%d",sum);
+
+    return 0;
+}
