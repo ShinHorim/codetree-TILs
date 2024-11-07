c45d320f4fc9330bbefcbe86f6917dbf60efcbd3
ShinHorim <capjang24@gmail.com>
2024-11-07 10:40:49 +0900

    Title: 숫자들의 배수, Time: 1082ms, Memory: 77MB, Status: Time Limit Exceed - CodeTree

--- 241107/숫자들의 배수/multiple-of-numbers.c
+++ 241107/숫자들의 배수/multiple-of-numbers.c
@@ -4,11 +4,11 @@ int main() {
     int a,b[1000];
     scanf("%d",&a);
 
-    printf("%d ",a);
-    int c = a+a;
+    int c = a;
     int i = 0;
     while(i<2){
-        if (c%5==0){
+        
+        if (a%5==0){
             i++;
         }
         printf("%d ", c);
